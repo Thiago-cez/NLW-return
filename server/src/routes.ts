da471027356e859209540553c1332d1ea700399e
Thiago-Cez <thiago.silva.ts804@gmail.com>
2022-05-30 13:54:28 +0100

    class 3

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -16,25 +16,20 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
+  const feedback =
+    /*ainda nao sei o que vai aqui!!*/
 
-  transport.sendMail({
-    from: "equipe Feedget<oi@feedget.com",
-    to: "Thiago Cezario <thiago.silva.ts804@gmail.com",
-    subject: "novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>tipo do feedback: ${type}</p>`,
-      `<p>Comentario: ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
+    transport.sendMail({
+      from: "equipe Feedget<oi@feedget.com",
+      to: "Thiago Cezario <thiago.silva.ts804@gmail.com",
+      subject: "novo feedback",
+      html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>tipo do feedback: ${type}</p>`,
+        `<p>Comentario: ${comment}</p>`,
+        `</div>`,
+      ].join("\n"),
+    });
 
   return res.status(201).json({
     data: feedback,
